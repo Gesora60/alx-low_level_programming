cf8877934560bb7fff21deaae4f4c49c203001c4
Gesora60 <mosmay60@gmail.com>
2023-09-12 11:52:05 -0700

    table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,23 @@
+#include "main"
+
+/**
+ *a function that prints the 9 times table, starting with 0.
+ *
+ *return: alays 0
+ */
+void times_table(void) {
+    int result;
+    for (int i = 0; i <= 10; i++) {
+        result = 9 * i;
+        if (i != 0) {
+            _putchar(',');
+            _putchar(' ');
+        }
+        if (result < 10) {
+            _putchar(' ');
+            _putchar(' ');
+        }
+        _putchar(result + '0');
+    }
+    _putchar('\n');
+}
