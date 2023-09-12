6a9e507e617c812517d8fe4838334f85ffb96b78
Gesora60 <mosmay60@gmail.com>
2023-09-12 08:04:51 -0700

    first function

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ *Print _putchar
+ *folloed by a new line
+ * 
+ *return (0)
+ */
+
+int main() {
+    putchar('_');
+    putchar('p');
+    putchar('u');
+    putchar('t');
+    putchar('c');
+    putchar('h');
+    putchar('a');
+    putchar('r');
+    putchar('\n');
+
+    return 0;
+}
