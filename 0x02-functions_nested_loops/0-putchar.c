4b5d4654111f6313bb6e95114b5fd09a1cbe635e
Gesora60 <mosmay60@gmail.com>
2023-09-12 08:31:50 -0700

    first function

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
 
 /**
- *Print _putchar
+ *function: print the phrase _putchar
+ *main: Print _putchar
  *folloed by a new line
  * 
  *return (0)
  */
 int main(void)
 {
-    putchar('_');
-    putchar('p');
-    putchar('u');
-    putchar('t');
-    putchar('c');
-    putchar('h');
-    putchar('a');
-    putchar('r');
-    putchar('\n');
+  putchar('_');
+  putchar('p');
+  putchar('u');
+  putchar('t');
+  putchar('c');
+  putchar('h');
+  putchar('a');
+  putchar('r');
+  putchar('\n');
 
-    return (0);
+  return (0);
 }
