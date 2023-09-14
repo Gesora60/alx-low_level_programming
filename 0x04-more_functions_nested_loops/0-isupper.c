fc5fab21f1904f16858001d7cdca9a4c371387aa
Gesora60 <mosmay60@gmail.com>
2023-09-14 10:12:41 -0700

    upper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -8,12 +8,10 @@
   */
 int _isupper(int x)
 {
-	if (c >= 'A' && c <= 'Z')
-	  {
-	    return (1);
-	      }
-	else
-	  {
-	    return (0);
-	  }
+	if (x >= 65 && x <= 90)
+	{
+		return (1);
+	}
+
+	return (0);
 }
