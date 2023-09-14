57e6e4ad9281991e0451431ed2b66191bb814c57
Gesora60 <mosmay60@gmail.com>
2023-09-14 11:27:31 -0700

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+/**
+  * more_numbers - Print 10 times the numbers since 0 up to 14
+  *
+  * Return: 10 times of the numbers since 0 up to 14
+  */
+void more_numbers(void)
+{
+  int y = 0;
+	while (y < 10)
+	{
+	    int x = 0;
+	for (; x <= 14; x++)
+	{
+	    if (x > 9)
+			{
+				putchar((x / 10) + '0');
+			}
+			putchar((x % 10) + '0');
+		}	
+	y++;
+	putchar('\n');
+	}
+}
