388af17485d1ccf0ac241f289a74195335d1b43d
Gesora60 <141133305+Gesora60@users.noreply.github.com>
2023-09-26 14:32:49 +0300

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -12,13 +12,10 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-  char *ptr = s;
-
-    while (n > 0) {
-        *ptr = b;
-        ptr++;
-        n--;
-    }
-
-    return s;
+	while (n)
+	{
+		s[n - 1] = b;
+		n--;
+	}
+	return (s);
 }
